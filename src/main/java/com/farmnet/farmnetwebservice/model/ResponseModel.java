9f93e04ee90438b789890dd5c3ef8869f871464f
miyandac <lornamiyanda@gmail.com>
2019-11-29 15:57:33 +0530

    fixed merge conflicts

--- src/main/java/com/farmnet/farmnetwebservice/model/ResponseModel.java
+++ src/main/java/com/farmnet/farmnetwebservice/model/ResponseModel.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -61,67 +60,3 @@ public class ResponseModel {
     
   
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.farmnet.farmnetwebservice.model;
-
-import java.util.Date;
-
-/**
- *
- * @author CHILIKWEM
- */
-public class ResponseModel {
-    
-    private String barcodenumber;
-    private String status;
-    private Date dateInserted;
-    private String comment;
-
-    public ResponseModel() {
-    }
-    
-    public String getBarcodenumber() {
-        return barcodenumber;
-    }
-
-    public void setBarcodenumber(String barcodenumber) {
-        this.barcodenumber = barcodenumber;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Date getDateInserted() {
-        return dateInserted;
-    }
-
-    public void setDateInserted(Date dateInserted) {
-        this.dateInserted = dateInserted;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "ResponseModel{" + "barcodenumber=" + barcodenumber + ", status=" + status + ", dateInserted=" + dateInserted + ", comment=" + comment + '}';
-    }
-    
-  
-}
->>>>>>> Farment rest web service
